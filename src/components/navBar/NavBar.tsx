478b7034f0dbd88506bc0e053295a10407741955
Douglas1705 <bertoldi_douglas@hotmail.com>
2024-09-26 15:44:49 -0300

    Classe nav criada

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -3,7 +3,7 @@ import UniversalButton from "../../common/UniversalButton";
 function NavBar() {
   return (
     <div className="bg-gray-950 flex flex-row p-2.5 justify-between">
-      <img src="https://www.max.com/img/max-h-w-l.svg" className="" alt="Logo-Max" />
+      <img src="https://www.max.com/img/max-h-w-l.svg" className="w-25" alt="Logo-Max" />
       <div>
         <UniversalButton
           title="ENTRAR"
