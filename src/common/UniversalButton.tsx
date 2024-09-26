f1667fd6882366b112ec61522fa8f51f77357a78
Douglas1705 <bertoldi_douglas@hotmail.com>
2024-09-26 15:34:38 -0300

    botao criado

--- src/common/UniversalButton.tsx
+++ src/common/UniversalButton.tsx
@@ -1,19 +1,49 @@
 interface ButtonProps {
-    title: string;
+  title: string;
+  backgroundColor?: "black" | "blue";
+  size?: "medium" | "large";
+  type?: "button" | "submit" | "reset";
+  name?: string;
+  onClick?: () => void;
 }
 
-function UniversalButton({title} : ButtonProps){
-    return(
-        <button className="
-        decoration-blue-700 
-        text-white 
-        p-2.5 
-        bg-blue-700
-        rounded-lg
-        "
-        >{title}</button>
-    )
+function UniversalButton({
+  title,
+  backgroundColor = "blue",
+  size = "medium",
+  type = "button",
+  name = "",
+  onClick,
+}: ButtonProps) {
+  const sizeClasses = {    
+    medium: "px-6 py-2 text-xs",
+    large: "px-8 py-3 text-md",
+  };
+  const backgroundClasses = {
+    black: "bg-gray-650 hover:bg-slate-900",
+    blue: "bg-blue-700 hover:bg-blue-900"
     
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      name={name}
+      type={type}
+      className={`
+                ${backgroundClasses[backgroundColor]} 
+                
+                
+                ${sizeClasses[size]} 
+                rounded-lg
+                font-medium
+                text-white 
+            `}
+      style={{ fontFamily: "Gilroy-Bold, sans-serif" }}
+    >
+      {title}
+    </button>
+  );
 }
 
-export default UniversalButton
+export default UniversalButton;
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -2,12 +2,17 @@ import UniversalButton from "../../common/UniversalButton";
 
 function NavBar() {
   return (
-    <div className="bg-gray-950">
-      <img src="https://www.max.com/img/max-h-w-l.svg" alt="" />
-      <UniversalButton title="ASSINE AGORA"></UniversalButton>
+    <div className="bg-gray-950 flex flex-row p-2.5 justify-between">
+      <img src="https://www.max.com/img/max-h-w-l.svg" className="" alt="Logo-Max" />
+      <div>
+        <UniversalButton
+          title="ENTRAR"
+          backgroundColor="black"
+        ></UniversalButton>
+        <UniversalButton title="ASSINE AGORA"></UniversalButton>
+      </div>
     </div>
-  
-  )
+  );
 }
 
 export default NavBar;
