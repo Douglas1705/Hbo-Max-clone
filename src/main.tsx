f0bec3a9b6cb2e7062fccde3b812120ed19bdbf6
Douglas1705 <bertoldi_douglas@hotmail.com>
2024-09-26 23:12:32 -0300

    aducionando arquivo nojekill

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ import Header from './components/navBar/header'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <NavBar />
-    <Header />
-   
+    <Header />   
   </StrictMode>,
 )
